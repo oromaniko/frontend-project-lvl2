c7a3223b5be76e98a96b1f104fe021a866628258
Olga Romaniko <oromaniko@gmail.com>
2021-12-01 20:19:29 +0300

    fix index.test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,12 @@
 /* eslint-disable quotes */
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import { readFileSync } from 'fs';
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
