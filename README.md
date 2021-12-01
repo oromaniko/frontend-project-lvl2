04e40ebaae4cf202b3e9d1bbebbd7c754ddfa547
Olga Romaniko <oromaniko@gmail.com>
2021-12-01 20:15:34 +0300

    fix index.test, add _dirname

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/oromaniko/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/oromaniko/frontend-project-lvl2/actions)
 
+[![Node CI](https://github.com/oromaniko/frontend-project-lvl2/actions/workflows/nodejs.yml/badge.svg)](https://github.com/oromaniko/frontend-project-lvl2/actions/workflows/nodejs.yml)
+
 ### CodeClimat
 [![Maintainability](https://api.codeclimate.com/v1/badges/cee8197b818dc797aacf/maintainability)](https://codeclimate.com/github/oromaniko/frontend-project-lvl2/maintainability)
 
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,18 @@
 /* eslint-disable quotes */
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFileSync } from 'fs';
 import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
-test('genDiff flat', () => {
-  const expected = JSON.stringify({
-    "- follow": false,
-    "  host": "hexlet.io",
-    "- proxy": "123.234.53.22",
-    "- timeout": 50,
-    "+ timeout": 20,
-    "+ verbose": true,
-  }, null, ' ').replaceAll('"', '');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-  expect(genDiff('/home/oulka/frontend-project-lvl2/src/file1.json', '/home/oulka/frontend-project-lvl2/src/file2.json')).toEqual(expected);
+test('genDiff flat', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  expect(genDiff(file1, file2)).toEqual(readFile('expected_file.txt'));
 });
