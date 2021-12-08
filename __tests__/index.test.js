fc9ec2b2a65b4197dfcddba46aa299c2a4115d54
Olga Romaniko <oromaniko@gmail.com>
2021-12-08 20:37:58 +0300

    make plain tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,8 +10,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff JSON-YAML', () => {
+test('genDiff stylish JSON-YAML', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.yml');
   expect(genDiff(file1, file2)).toEqual(readFile('expected_file.txt'));
+});
+
+test('genDiff plain JSON-YAML', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.yml');
+  expect(genDiff(file1, file2, 'plain')).toEqual(readFile('expected_plain.txt'));
 });
